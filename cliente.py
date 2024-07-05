ac872851bedf7eacb502ccdb0b138fdbf362d869
Lucas Quadros <lucaspquadros@gmail.com>
2024-07-04 21:29:05 -0300

    chat de texto melhorado

--- cliente.py
+++ cliente.py
@@ -1,12 +1,14 @@
 import zmq
 import threading
+import time
+from functools import partial
 
 def send_video():
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
     socket.connect("tcp://localhost:5555")
     while True:
-        video_data = b"video frame"  # Placeholder for actual video data
+        video_data = b"video"  # Implementar video
         socket.send(video_data)
 
 def receive_video():
@@ -23,7 +25,7 @@ def send_audio():
     socket = context.socket(zmq.PUB)
     socket.connect("tcp://localhost:5557")
     while True:
-        audio_data = b"audio frame"  # Placeholder for actual audio data
+        audio_data = b"audio"  # Implementar audio
         socket.send(audio_data)
 
 def receive_audio():
@@ -35,32 +37,51 @@ def receive_audio():
         audio_data = socket.recv()
         #print("Received audio data")
 
-def send_text(username):
+def send_text(username, selected_channels):
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
     socket.connect("tcp://localhost:5559")
+    channel = None
+    text_data = None
+    print("Digite .t para trocar de canal de texto\n")
     while True:
-        text_data = input(" ")
-        socket.send(f"{username}: {text_data}".encode('utf-8'))
+        time.sleep(0.05)
+        if channel == None:
+            channel = input("Selecione o canal para enviar sua mensagem: \n")
+        if channel not in selected_channels:
+            print(f"Canal {channel} inválido")
+            channel = None
+            continue
+        text_data = input("Digite sua mensagem: \n")
+        if text_data == ".t":
+            channel = None
+            continue
+        socket.send(f"{channel} - {username}: {text_data}".encode('utf-8'))
 
-def receive_text():
+def receive_text(selected_channels):
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
     socket.connect("tcp://localhost:5560")
-    socket.setsockopt_string(zmq.SUBSCRIBE, "")
+    for channel in selected_channels:
+        socket.setsockopt_string(zmq.SUBSCRIBE, channel)
     while True:
         text_data = socket.recv().decode('utf-8')
-        #print("Received text data")
         print(text_data)
 
 if __name__ == "__main__":
-    username = input("Enter username: ")
+    username = input("Insira seu nome de usuário: ")
+    text_channels = ["t1", "t2", "t3"]
+    audio_channels = ["a1", "a2", "a3"]
+    video_channels = ["v1", "v2", "v3"]
+    print(f"Canais disponíveis:\n Texto: {text_channels}\n Audio: {audio_channels}\n Video:{video_channels}")
+    selected_channels = input("Insira os canais aos quais deseja se inscrever (separados por espaço): ").split()
+    
     video_send_thread = threading.Thread(target=send_video)
     video_receive_thread = threading.Thread(target=receive_video)
     audio_send_thread = threading.Thread(target=send_audio)
     audio_receive_thread = threading.Thread(target=receive_audio)
-    text_send_thread = threading.Thread(target=send_text, args=(username,))
-    text_receive_thread = threading.Thread(target=receive_text)
+    text_send_thread = threading.Thread(target=send_text, args=(username, selected_channels))
+    text_receive_thread = threading.Thread(target=partial(receive_text, selected_channels))
 
     video_send_thread.start()
     video_receive_thread.start()
